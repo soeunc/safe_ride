9d6358d0641961822ecb2291e5aaf562460d9434
soeunc <fkckxk3910@gmail.com>
2024-04-11 18:37:43 +0900

    init

---
+++ src/main/java/com/example/safe_ride/SafeRideApplication.java
@@ -0,0 +1,13 @@
+package com.example.safe_ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/safe_ride/SafeRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safe_ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
