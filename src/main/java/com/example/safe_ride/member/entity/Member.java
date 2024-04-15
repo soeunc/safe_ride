a108ff0a28b9d7e52ad114acd5209a970cbb3e5c
“mafatofu” <qwqw0310@gmail.com>
2024-04-15 09:36:48 +0900

    feat: authority 컬럼 추가
    spring security로 로그인 기능 작성 시 권한 확인이 필수적임에 따라 추가.

--- src/main/java/com/example/safe_ride/member/entity/Member.java
+++ src/main/java/com/example/safe_ride/member/entity/Member.java
@@ -1,14 +1,14 @@
 package com.example.safe_ride.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.safe_ride.myPage.entity.Badge;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Getter
 @Entity
@@ -27,4 +27,6 @@ public class Member {
     private String nickname;
     private String phoneNumber;
     private String birthday;
+    private Authority authority;
+
 }
