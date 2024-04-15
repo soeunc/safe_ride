3deeddaabf80e3e0af11b57c5b0ff5f39638e6a4
soeunc <fkckxk3910@gmail.com>
2024-04-15 17:18:23 +0900

    fix: 네이버 맵 사용자 위치 정보 받기

---
+++ src/main/java/com/example/safe_ride/safe/service/NcpMapApiService.java
@@ -0,0 +1,19 @@
+package com.example.safe_ride.safe.service;
+
+import com.example.safe_ride.safe.dto.geocoding.GeoNcpResponse;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.Map;
+
+// http 요청 보내는 클래스
+public interface NcpMapApiService {
+    // 구현체를 만들어서 http 요청을 보내는 프록시 객체를 만들어 줘야 사용가능하다.
+
+    // Geocode
+    @GetExchange("/map-geocode/v2/geocode")
+    GeoNcpResponse geocode(
+            @RequestParam
+            Map<String, Object> params
+    );
+}
