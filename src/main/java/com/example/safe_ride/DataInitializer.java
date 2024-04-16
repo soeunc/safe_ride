89cbda5cd1bcdc97a45fec9b76a354d651274751
seoju <mmm5885@naver.com>
2024-04-16 09:12:00 +0900

    Fix: 테이블 삭제 안되던 오류 수정

--- src/main/java/com/example/safe_ride/DataInitializer.java
+++ src/main/java/com/example/safe_ride/DataInitializer.java
@@ -26,13 +26,11 @@ public class DataInitializer {
     public void loadData() {
         try {
             // 데이터베이스 연결
-            String url = "jdbc:sqlite:path_to_your_database.db";
+            String url = "jdbc:sqlite:db.sqlite";
             try (Connection conn = DriverManager.getConnection(url);
                  Statement stmt = conn.createStatement()) {
-                System.out.println("loadData 정상 실행");
-//                // 테이블 데이터 삭제
-//                stmt.execute("DELETE FROM location_infor");
-//                System.out.println("table 삭제 완료");
+                // 테이블 데이터 삭제
+                stmt.execute("DELETE FROM location_infor");
                 // 데이터 로딩 로직
                 List<LocationInfor> addresses = new CsvDataReader().readCsvData("src/main/resources/address.csv");
                 databaseLoader.insertData(addresses);
