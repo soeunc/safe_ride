ecefde56420a542c6713ba6c359981fd099d754f
seoju <mmm5885@naver.com>
2024-04-23 17:31:03 +0900

    Feat : ncp Geolocation 설정

--- src/main/java/com/example/safe_ride/config/NcpClientConfig.java
+++ src/main/java/com/example/safe_ride/config/NcpClientConfig.java
@@ -27,6 +27,7 @@ public class NcpClientConfig {
     // NCP Map Rest Client
     private static final String NCP_APIGW_KEY_ID = "X-NCP-APIGW-API-KEY-ID";
     private static final String NCP_APIGW_KEY = "X-NCP-APIGW-API-KEY";
+
     // geoLocation
     private static final String X_TIMESTAMP_HEADER = "x-ncp-apigw-timestamp";
     private static final String X_IAM_ACCESS_KEY = "x-ncp-iam-access-key";
@@ -38,10 +39,14 @@ public class NcpClientConfig {
     @Value("${ncp.api.client-secret}")
     private String ncpMapClientSecret;
     // geolocation
-    @Value("70mgu2av7k")
-    private String ncpGeoloClientId;
-    @Value("$jw2D5LwBQBwfTF3fNVQxadiaofQWubSsKL7ukuCS")
-    private String ncpGeoloClientSecret;
+    // 사용자 인증 오류로 ncp 이용 보류
+    // yaml 변경해야해서 우선 하드코딩
+//    @Value("${ncp.api.api-access}")
+    @Value("ZYkO4DEv8pyb18nyqlfk")
+    private String accessKey;
+//    @Value("${ncp.api.api-secret}")
+    @Value("uT7hWH7wxUkhjzyKDOag9GYgL6MPcXUzZ8Wd1Syw")
+    private String secretKey;
 
     // map
     @Bean
@@ -65,8 +70,8 @@ public class NcpClientConfig {
                     //1. 요청을 보내는 Unix Time
                     long timestamp = System.currentTimeMillis();
                     requestHeaders.add(X_TIMESTAMP_HEADER, Long.toString(timestamp));
-                    //2. Acess key
-                    requestHeaders.add(X_IAM_ACCESS_KEY, ncpGeoloClientId);
+                    //2. Access key
+                    requestHeaders.add(X_IAM_ACCESS_KEY, accessKey);
                     //3. 현재시각 + 요청 URI + 요청 메서드 정보로 만드는 시그니처
                     //메서드로 만들어 넣기
                     //시그니처를 만드는데 필요한 정보는 request에 있다.
@@ -91,7 +96,7 @@ public class NcpClientConfig {
 
     // geoloacation
     @Bean
-    public GeoLocationInterface geolocationService(){
+    public GeoLocationInterface geoLocationInterface(){
         return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(ncpGeolocationClient()))
                 .build()
                 .createClient(GeoLocationInterface.class);
@@ -110,11 +115,11 @@ public class NcpClientConfig {
                 .append(newLine)
                 .append(timestamp)
                 .append(newLine)
-                .append(ncpGeoloClientId)
+                .append(accessKey)
                 .toString();
 
         try {
-            SecretKeySpec signingKey = new SecretKeySpec(ncpGeoloClientSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+            SecretKeySpec signingKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
             Mac mac = Mac.getInstance("HmacSHA256");
             mac.init(signingKey);
 
