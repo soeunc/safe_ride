1a1bfe678172bba19472851e68b23b9c5bf4de9c
“mafatofu” <qwqw0310@gmail.com>
2024-04-25 15:22:02 +0900

    Fix : 주간 라이딩 기록에 타지 않은 날은 0으로 표시되도록 변경
    기존에는 데이터를 가져와, 그 데이터 수만큼 순서대로 화면에 뿌렸음.
    -> 21/23일에 라이딩을 했다면 화면상에는 21/22일에 라이딩 한 기록으로 나타남.
    정상적으로 라이딩한 날짜에 데이터가 뿌려지고, 라이딩하지 않은 날은 데이터가 0으로 나타나도록 수정

--- src/main/java/com/example/safe_ride/myPage/service/MyPageService.java
+++ src/main/java/com/example/safe_ride/myPage/service/MyPageService.java
@@ -4,6 +4,7 @@ import com.example.safe_ride.member.dto.BadgeDto;
 import com.example.safe_ride.member.entity.Badge;
 import com.example.safe_ride.member.repo.BadgeRepo;
 import com.example.safe_ride.myPage.dto.MyPageDto;
+import com.example.safe_ride.myPage.dto.WeeklyRecordDto;
 import com.example.safe_ride.myPage.entity.MyPage;
 import com.example.safe_ride.myPage.repo.MyPageRepo;
 import com.example.safe_ride.myPage.repo.MyPageRepoDsl;
@@ -17,9 +18,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static java.lang.String.format;
 
@@ -56,7 +55,7 @@ public class MyPageService {
         //이번주 날짜(일~토)
         myPageDto.setThisWeek(getThisWeekDayList());
         //주간 개별 기록
-        myPageDto.setWeeklyRecordList(findWeeklyRecordList(memberId));
+        myPageDto.setWeeklyRecordList(weeklyRecordListTotal(getThisWeekDayList(),findWeeklyRecordList(memberId)));
 
         return myPageDto;
     }
@@ -126,18 +125,77 @@ public class MyPageService {
         
     }
     //주간개별기록
-    public List<Integer> findWeeklyRecordList(Long memberId){
-        List<Integer> weeklyRecordList = new ArrayList<>();
+    public List<WeeklyRecordDto> findWeeklyRecordList(Long memberId){
+        List<WeeklyRecordDto> weeklyRecordList = new ArrayList<>();
         LocalDateTime today = LocalDate.now().atStartOfDay();
+        LocalDateTime todayEnd = LocalDateTime.of(today.getYear(),today.getMonth(), today.getDayOfMonth(), 23,59,59);
         int day = today.get(ChronoField.DAY_OF_WEEK);
         if (day == 7) day = 0;
-        //이번주 시작일자(일요일)
+        //이번주 시작일자(일요일 00:00:00)
         LocalDateTime start = today.minusDays(day);
-        //이번주 끝일자
-        LocalDateTime end = start.plusDays(6);
+        //이번주 끝일자(토요일 23:59:59)
+        LocalDateTime endTemp = todayEnd.minusDays(day);
+        LocalDateTime end = endTemp.plusDays(6);
         weeklyRecordList = myPageRepoDsl.getWeeklyRecordList(memberId,start,end);
         return weeklyRecordList;
     }
+    //이번주 라이딩 기록 총합버전
+    //이번주 날짜와 이번주 기록을 받아 주단위 기록을 생성
+    //특정 날짜 기록이 없다면 0으로 변경
+    public List<WeeklyRecordDto> weeklyRecordListTotal(
+            List<String> thisWeekDayList,
+            List<WeeklyRecordDto> weeklyRecordList
+    ){
+        List<WeeklyRecordDto> weeklyRecordDtoList = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        DateTimeFormatter dayEqualCk = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        //날짜비교.
+        for (int i = 0; i < thisWeekDayList.size(); i++) {
+            WeeklyRecordDto dto = new WeeklyRecordDto();
+            LocalDateTime dateTime;
+
+            // DB에서 받아온 데이터list가 비어서 더이상 2중for문을 돌지않을때
+            // 그냥 빈 값 만들어준다
+            if(weeklyRecordList.isEmpty()){
+                //해당 날짜와 기록을 dto에 저장
+                //parsing을 위한 변경
+                String oneDay = thisWeekDayList.get(i) + " 00:00:00.000";
+                dateTime = LocalDateTime.parse(oneDay, formatter);
+                dto.setCreateDate(dateTime);
+                dto.setTodayRecord(0);
+            }
+
+            //해당 하는 날짜가 있고 DB에서 받아온 데이터list가 비어있지 않을 때
+            for (int j = 0; j < weeklyRecordList.size(); j++) {
+                if (
+                        thisWeekDayList.get(i).equals(
+                                weeklyRecordList.get(j).getCreateDate().format(dayEqualCk)
+                        )
+                ){
+                    //해당 날짜와 기록을 dto에 저장(DB값 그대로)
+                    dto.setCreateDate(weeklyRecordList.get(j).getCreateDate());
+                    dto.setTodayRecord(weeklyRecordList.get(j).getTodayRecord());
+                    //저장 후 list에서 해당 데이터 삭제
+                    weeklyRecordList.remove(j);
+
+                    break;
+                } else {
+                    //해당 날짜와 기록을 dto에 저장
+                    //parsing을 위한 변경
+                    String oneDay = thisWeekDayList.get(i) + " 00:00:00.000";
+                    dateTime = LocalDateTime.parse(oneDay, formatter);
+                    dto.setCreateDate(dateTime);
+                    dto.setTodayRecord(0);
+
+                    break;
+                }
+            }
+            weeklyRecordDtoList.add(dto);
+        }
+        return weeklyRecordDtoList;
+    }
+
     //전체기록합산결과
     public int findTotalRecord(Long memberId){
         return myPageRepoDsl.getTotalRecord(memberId);
